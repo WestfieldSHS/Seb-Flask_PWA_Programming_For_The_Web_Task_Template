05f7fcdf3adbcaf2c8f050dbc917f0e9732da386
SEBASTIAN SHELTON <sebastian.shelton@education.nsw.gov.au>
2026-02-03 12:44:19 +1100

    initial commit after new git repo

---
+++ .database/my_queries.sql
@@ -0,0 +1,9 @@
+-- CREATE TABLE Records(Record_id,Record_name, email);
+
+-- INSERT INTO Records(Record_id,Record_name,email) 
+-- VALUES (1,"disstrack","smth@writa"), (2,"dattrack","smth@writa"), (3,"moretrack","mrth@writa");
+
+
+-- SQL QUERIES
+-- SELECT * FROM Records WHERE Record_id = 1 OR 2 OR 3
+
