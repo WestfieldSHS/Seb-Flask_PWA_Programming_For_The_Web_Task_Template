ba0a44a1afc8900d4c56c6a4e3fbe7728f31b789
SEBASTIAN SHELTON <sebastian.shelton@education.nsw.gov.au>
2026-02-04 10:56:11 +1100

    anotherfinalpush

--- importantstuff.py
+++ importantstuff.py
@@ -1,7 +1,7 @@
 
 #PUSHING TO GITHUB
 #git status
-#git add (files or just -m)
+#git add (files or just .)
 #git remote add Seb-Flask_PWA_Programming_For_The_Web_Task_Template https://github.com/WestfieldSHS/Seb-Flask_PWA_Programming_For_The_Web_Task_Template
 #git commit -m |add a message to the commit|
 #git push
@@ -11,4 +11,4 @@
 #venv\Scripts\Activate.ps1
 
 #GETTING FLASK
-#flask run      (uses app.py and index.html file in templates)
+#flask run      (only in powershell, uses app.py and index.html file in templates)
