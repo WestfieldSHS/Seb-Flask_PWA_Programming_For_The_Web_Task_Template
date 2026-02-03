eaea8f9da1aedab524eccd523ae69b8203ad71c3
SEBASTIAN SHELTON <sebastian.shelton@education.nsw.gov.au>
2026-02-04 10:43:24 +1100

    committest

---
+++ importantstuff.py
@@ -0,0 +1,13 @@
+
+#PUSHING TO GITHUB
+#git status
+#git commit -m
+#git remote add Seb-Flask_PWA_Programming_For_The_Web_Task_Template https://github.com/WestfieldSHS/Seb-Flask_PWA_Programming_For_The_Web_Task_Template
+#git push
+
+#ACTIVATING VENV
+#for powershell
+#venv\Scripts\Activate.ps1
+
+#GETTING FLASK
+#flask run      (uses app.py and index.html file in templates)
