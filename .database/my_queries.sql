f03ff92f557d9cadd6b615e5f389a69b991349a1
sebastian shelton <sebastian.shelton@education.nsw.gov.au>
2026-02-14 23:42:12 +1100

    seoncdlastday

--- .database/my_queries.sql
+++ .database/my_queries.sql
@@ -1,9 +1,5 @@
--- CREATE TABLE Records(Record_id,Record_name, email);
+CREATE TABLE IF NOT EXISTS Records(Record_id,Record_name, email);
 
--- INSERT INTO Records(Record_id,Record_name,email) 
--- VALUES (1,"disstrack","smth@writa"), (2,"dattrack","smth@writa"), (3,"moretrack","mrth@writa");
-
-
--- SQL QUERIES
--- SELECT * FROM Records WHERE Record_id = 1 OR 2 OR 3
-
+INSERT INTO 
+--m SQL QUERIES
+SELECT * FROM Records WHERE Record_id = 1 OR 2 OR 3
